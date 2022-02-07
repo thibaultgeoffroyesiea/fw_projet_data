cc2b95c087523dc3fd6586285c3b725e26836995
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2022-02-07 17:04:32 +0100

    ajustements label_name

--- projets/TEST_PROJET/eval.py
+++ projets/TEST_PROJET/eval.py
@@ -8,7 +8,7 @@ df = pd.read_csv(sys.argv[1])
 
 result_df = pd.DataFrame()
 
-result_df["prediction"] = [1,2,3,4,5]
+result_df["target"] = [1,2,3,4,5]
 
 
-result_df.to_csv(sys.argv[0] + "result.csv")
+result_df.to_csv(sys.argv[0] + "/result.csv")
