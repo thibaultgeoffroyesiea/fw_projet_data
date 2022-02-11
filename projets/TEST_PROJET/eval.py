6caeeb67e93893d9508f39abff1fb8573ef370bc
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2022-02-11 14:50:32 +0100

    adding test_data and finishing eval script

--- projets/TEST_PROJET/eval.py
+++ projets/TEST_PROJET/eval.py
@@ -3,12 +3,17 @@ import pandas as pd
 import os
 
 
+script_path = sys.argv[0]
+
 df = pd.read_csv(sys.argv[1])
 
+img_list = os.listdir(sys.argv[2])
 
 result_df = pd.DataFrame()
 
-result_df["target"] = [1,2,3,4,5]
+result_df["target"] = [3,8,3,5,5,4,7,8,1,5,5,1,3]
 
 
-result_df.to_csv(sys.argv[0] + "/result.csv")
+result_df.to_csv(script_path + "/result.csv")
+
+
